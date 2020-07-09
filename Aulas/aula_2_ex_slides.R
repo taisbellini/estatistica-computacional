b1b75537a29d363417f55be29803ca099f91468f
Tais Bellini <tais38@gmail.com>
2020-07-09 07:48:44 -0300

    Exercicio 2

--- Aulas/aula_2_ex_slides.R
+++ Aulas/aula_2_ex_slides.R
@@ -23,3 +23,26 @@ tic("apply")
 apply(mapply(rnorm, n=1000, mean=1:200), 1, var)
 toc()
 
+#ex 2
+tic("total for")
+tic("create matrix")
+ms = list(10)
+for (i in 1:10){
+  m = matrix(nrow=20, ncol=100)
+  means = list(20)
+  for (j in 1:20){
+    m[j, ] = rnorm(100, j, i)
+    means[[j]] = mean(m[j,])
+  }
+  ms[[i]] = means
+}
+toc()
+toc()
+
+tic("total apply")
+msa = list(10)
+for(i in 1:10){
+  msa[[i]] = apply(mapply(rnorm, n=100, mean=1:20), 2, mean)
+}
+toc()
+
