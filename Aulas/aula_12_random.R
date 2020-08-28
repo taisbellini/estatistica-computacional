639bfc54ef0d21371e55e2f4aff117113c295c71
Tais Bellini <tais38@gmail.com>
2020-08-28 09:39:22 -0300

    Comments na aula 12

--- Aulas/aula_12_random.R
+++ Aulas/aula_12_random.R
@@ -1,21 +1,29 @@
 #Gerador de numeros aleatorios
 
-### Testar o primeiro gerador:
+#GLC: wi = (a*wi-1+b)%%m , i = 1,...,n
+#precisa ter o w0 para gerar os proximos
 
+
+# Exercicio 1: m=32, a=5, b=3, w0=11, n=m
 m = 32   
 a = 5
 b = 3
 w0 = 11
 n = m
+
+#vetor que vai guardar os resultados
 w = rep(0,n)
+#cria o primeiro
 w[1] = (a*w0 + b) %% m
 
 for(i in 1:(n-1)){
   w[(i+1)] = (a*w[i] + b) %% m
 }
 
+w
 
-### A função
+
+# Exercicio 2: criar uma função
 
 glc =function(w0,m,a,b,n){
   w = rep(0, n)
@@ -24,44 +32,45 @@ glc =function(w0,m,a,b,n){
     w[(i+1)] = (a*w[i] + b) %% m
   }
   return(w)
-  
 }
 
 
 w.ex1 =glc(w0=11,m=32,a=5,b=3,n=32)
 
 
-# Considerando o segundo conjunto de valores:
-
+# Exercicio 3:
 w.ex3 =glc(w0=20,m=32,a=7,b=13,n=32)
 
+w.ex1
+w.ex3 # este se repete a partir de 8. A parametrizacao a, b, m eh muito importante para um gerador bom
 
-## Sequencia uniforme
+
+#Exercicio 4: Sequencia uniforme
 glc.u = function(w0,m,a,b,n){
-  
-  #completar
-  
+  w = rep(0, n)
+  w[1] = (a*w0 + b) %% m
+  for (i in 1:(n-1)){
+    w[(i+1)] = (a*w[i] + b) %% m
+  }
+  w.u = w/m
+  return(list("seq" = w, "unif" = w.u))
 }
-
 
 ## Comparando resultados
 
 w1 = glc.u(w0=13234,m=2^32,a=69069,b=23606797,n=5000)
 w2 = glc.u(w0=13234,m=2^32,a=69068,b=23606797,n=5000)
 
-
-
 ## Olhar histograma
 
-
+#para colocar os graficos lado a lado
 par(mfrow=c(1,2))
+
 hist(w1$unif)
 hist(w2$unif)
 
 
 ## Diagrama de dispersao
-
-
 
 par(mfrow=c(1,2))
 plot(w1$unif)
@@ -75,13 +84,15 @@ summary(w2$unif)
 
 ## ACF
 
-
 par(mfrow=c(1,2))  
 acf(w1$unif,lag.max=20)
 acf(w2$unif,lag.max=20)
 
 ## Kolmogorov-Smirnov
 
-
 ks.test(w1$unif,"punif",0,1)
 ks.test(w2$unif,"punif",0,1)
+
+w.runif = runif(1000)
+
+ks.test(w.runif, "punif", 0.1)
