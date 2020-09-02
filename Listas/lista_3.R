1acc05cb17e83838607cbcab3c86125588a05b50
Tais Bellini <tais38@gmail.com>
2020-09-02 10:58:13 -0300

    Q2

--- Listas/lista_3.R
+++ Listas/lista_3.R
@@ -66,7 +66,7 @@ hist(F.var)
 
 ks.test(F.var, df(F.var, d1, d2))
 
-#d - Binomial Negativa 
+# d - Binomial Negativa 
 
 k = 5
 n = 100 
@@ -86,3 +86,49 @@ bn = BN.generator(k,n,p)
 # e - Multivariada
 
 
+## Exercicio 2
+
+# b
+
+unif.generator = function(a, b, n){
+  u = runif(n)
+  x = a + b*u
+  return(x)
+}
+
+# Gerar U[a-1/2;a+1/2] a partir de U[0,1]
+# fx = 1*f0(x-(a+1/2))
+# define a = 2
+
+n = 1000
+a = 2 - (1/2) # o a da locacao
+b = 1 
+
+unif.1 = unif.generator(a, b, n)
+hist(unif.1)
+summary(unif.1)
+
+# Gerar U[0;b] a partir de U[0,1]
+# fx = 1/b(f(x/b))
+# a = 0, define b = 4
+
+a = 0
+b = 4
+
+unif.2 = unif.generator(a, b, n)
+hist(unif.2)
+
+# Gerar U[a;b] a partir de U[0,1]
+# fx = (1/(b-a))f0(x-a/(b-a))
+# define a = 2, b = 4 
+# a = a
+# b = b-a
+
+adef = 2
+bdef = 4
+
+a = adef
+b = bdef-adef
+
+unif.3 = unif.generator(a, b, n)
+hist(unif.3)
